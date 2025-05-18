94d83c02590eb2a7232111e1b46244017df9a73d
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2025-05-18 23:26:40 +0530

    Added logout functionality

--- lib/select.dart
+++ lib/select.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:app/login.dart';
 import 'package:app/technicians.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
@@ -111,6 +113,38 @@ class _selectState extends State<select> {
           ),
         ),
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Text(
+                'Profile',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () async {
+                Navigator.pop(context); // Close drawer first
+                await FirebaseAuth.instance.signOut(); // Sign out user
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => login()),
+                ); // Navigate to login screen
+              },
+
+            ),
+          ],
+        ),
+      ),
+
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Container(
